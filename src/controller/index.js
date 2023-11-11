8f6be06fc438901974fd45e67f491a8d2b5211e6
ryan-dia <cjfdnjs158@naver.com>
2023-11-11 16:26:12 +0900

    에러 발생시 에러메시지를 출력하고 다시 식당 방문 날짜 입력 받는다

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,26 +1,47 @@
+import Date from '../model/Date.js';
 import ChristmasModel from '../model/index.js';
 import InputView from '../view/InputView.js';
+import OutputView from '../view/OutputView.js';
 
 class ChristmasController {
   #model;
 
   #inputView;
 
+  #outputView;
+
   constructor() {
     this.#model = new ChristmasModel();
     this.#inputView = InputView;
+    this.#outputView = OutputView;
   }
 
   async run() {
-    const date = await this.#readDate();
-
-    this.#model.validateDate(date);
+    const order = await this.#receiveOrder();
   }
 
-  async #readDate() {
-    const date = await this.#inputView.readDate();
+  async #receiveOrder() {
+    const date = await this.#getDate();
 
-    return Number(date);
+    return { date };
+  }
+
+  async #getDate() {
+    try {
+      const date = await this.#inputView.readDate();
+
+      return Date.of(Number(date));
+    } catch ({ message }) {
+      return this.#onError(message, 'date');
+    }
+  }
+
+  async #onError(message, process) {
+    this.#outputView.print(message);
+
+    if (process === 'date') {
+      await this.#getDate();
+    }
   }
 }
 
