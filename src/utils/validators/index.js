93b669810f80764bd3303a583bf76666492f32d2
ryan-dia <cjfdnjs158@naver.com>
2023-11-11 23:11:57 +0900

    feat: 하나의 파일에서 validator export

--- src/utils/validators/index.js
+++ src/utils/validators/index.js
@@ -7,6 +7,8 @@ export { default as isEmptyString } from './src/is-empty-string/index.js';
 export { default as isNan } from './src/is-nan/index.js';
 export { default as isInteger } from './src/is-integer/index.js';
 export { default as isNumberValidScope } from './src/is-number-valid-scope/index.js';
+export { default as isValidCount } from './src/is-valid-count/index.js';
+export { default as isValidFormatter } from './src/is-valid-formatter/index.js';
 
 /**
  * @param {string} input
