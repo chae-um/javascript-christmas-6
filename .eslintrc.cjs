e06b93e957c5bb449546a87e6c5129dcdca9cdb0
ryan-dia <cjfdnjs158@naver.com>
2023-11-13 22:35:31 +0900

    chore: eslint 규칙 변경
    class-methods-use-this : off 로 변경
    import/prefer-default-export : off로 변경
    이유는 eslintrc.cjs 파일에서 해당 규칙위 주석 참고

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,15 @@ module.exports = {
     // https://github.com/woowacourse-precourse/javascript-racingcar-6/pull/4
     'import/extensions': ['error', 'always', { ignorePackages: true }],
 
+    // static을 사용하지 말하야 하는 곳도 에러 표시됨 따라서 eslint 규칙에 도움 받지 않고 자의적 판단
+    // airbnb style guide 설명 : 인스턴스 메서드라는 것은 수신자의 속성에 따라 다르게 동작한다는 것을 나타내야 합니다.
+    // https://github.com/airbnb/javascript#classes--methods-use-this
+    'class-methods-use-this': 'off',
+
+    // 구현 완료후 규칙 적용 예정
+    // https://github.com/airbnb/javascript#modules--prefer-default-export
+    'import/prefer-default-export': 'off',
+
     // 규칙 추가
 
     // async/await 실수 방지
