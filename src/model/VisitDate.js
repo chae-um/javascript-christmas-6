cce29f97966811574293dae35721436f47f2160e
ryan-dia <cjfdnjs158@naver.com>
2023-11-15 01:25:17 +0900

    style(model): VisitDate 클래스 메서드에 JSDoc 타입 주석 추가

--- src/model/VisitDate.js
+++ src/model/VisitDate.js
@@ -5,16 +5,25 @@ import { handleValidationError } from '../utils/error/index.js';
 import { isInteger, isNan, isNumberValidScope } from '../utils/validators/index.js';
 
 class VisitDate {
+  /**
+   * @private
+   * @type {number}
+   */
   #date;
 
   /**
-   * @param {number} date
+   * @param {number} date - 유저가 방문하기 원하늘 날짜
    */
   constructor(date) {
     this.#validate(date);
+
     this.#date = date;
   }
 
+  /**
+   * @private
+   * @param {number} date
+   */
   #validate(date) {
     if (isNan(date)) {
       handleValidationError(ERROR_MESSAGE.invalidDate);
@@ -27,18 +36,38 @@ class VisitDate {
     }
   }
 
+  /**
+   * @static
+   * @param {number} date
+   * @returns {VisitDate}
+   */
   static of(date) {
     return new VisitDate(date);
   }
 
+  /**
+   * @public
+   * @returns {boolean}
+   */
   isChristmasDDayDiscount() {
     return this.#date >= EVENT_DATE.christmasDDayStart && this.#date <= EVENT_DATE.christmasDDayEnd;
   }
 
+  /**
+   * @public
+   * @returns {number}
+   */
   getDate() {
     return this.#date;
   }
 
+  /**
+   * 평일/주말 할인이 가능한지 판별해주는 메서드
+   * @public
+   * @param {number} month
+   * @param {number} year
+   * @returns {boolean}
+   */
   isWeekdays(month = EVENT_DATE.month, year = EVENT_DATE.year) {
     const { days } = EVENT_DATE;
     const specificDate = new Date(year, month - 1, this.#date);
@@ -47,6 +76,10 @@ class VisitDate {
     return Boolean(days[dayIndex]);
   }
 
+  /**
+   * @public
+   * @returns {boolean}
+   */
   isSpecialDiscount() {
     const { specialDay } = EVENT_DATE;
 
