e5dcacf3b588ccf683e40df733d2e4becd7d5ec1
ryan-dia <cjfdnjs158@naver.com>
2023-11-15 01:48:53 +0900

    fix(utils): 에러 모듈에서 handleValidationError를 불러오는 import문 재구성

--- src/utils/validators/index.js
+++ src/utils/validators/index.js
@@ -1,6 +1,6 @@
 import { ERROR_MESSAGE } from '../../constants/Messages.js';
 
-import handleValidationError from '../error/index.js';
+import { handleValidationError } from '../error/index.js';
 import isEmptyString from './src/is-empty-string/index.js';
 
 export { default as isEmptyString } from './src/is-empty-string/index.js';
