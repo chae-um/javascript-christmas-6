e797c254aebffb92d51cdfa9aadd6ebc3fa5f11e
ryan-dia <cjfdnjs158@naver.com>
2023-11-15 23:08:23 +0900

    docs: Discount 객체에 JSDoc 타입 주석 추가

--- src/model/Discount.js
+++ src/model/Discount.js
@@ -1,8 +1,18 @@
 import { BADGE, DISCOUNT } from '../constants/System.js';
+import UserRequestedMenus from './UserRequestedMenus.js';
+import VisitDate from './VisitDate.js';
 
 class Discount {
+  /**
+   * @private
+   * @type {VisitDate}
+   */
   #visitDate;
 
+  /**
+   * @private
+   * @type {UserRequestedMenus}
+   */
   #userRequestedMenus;
 
   constructor(visitDate, userRequestedMenus) {
@@ -10,10 +20,22 @@ class Discount {
     this.#userRequestedMenus = userRequestedMenus;
   }
 
+  /**
+   * @returns {boolean}
+   */
   canDiscount() {
     return this.#userRequestedMenus.canDiscount();
   }
 
+  /**
+   * @returns {object}
+   * @property {object} weeklyDiscount
+   * @property {string} weeklyDiscount.weekly
+   * @property {number} weeklyDiscount.amount
+   * @property {number} specialDiscount
+   * @property {number} dDayDiscount
+   * @property {number} giftDiscount
+   */
   calculateDiscount() {
     const weeklyDiscount = this.#calculateWeeklyDiscount();
     const specialDiscount = this.#calculateSpecialDisCount();
@@ -23,6 +45,9 @@ class Discount {
     return { weeklyDiscount, specialDiscount, dDayDiscount, giftDiscount };
   }
 
+  /**
+   * @returns {number}
+   */
   calculateTotalBenefitsAmount() {
     const { weeklyDiscount, specialDiscount, dDayDiscount, giftDiscount } =
       this.calculateDiscount();
@@ -30,6 +55,9 @@ class Discount {
     return weeklyDiscount.amount + specialDiscount + dDayDiscount + giftDiscount;
   }
 
+  /**
+   * @returns {number}
+   */
   calculateDiscountedTotalMenuPrice() {
     const { weeklyDiscount, specialDiscount, dDayDiscount } = this.calculateDiscount();
     const discountSum = weeklyDiscount.amount + specialDiscount + dDayDiscount;
@@ -37,6 +65,9 @@ class Discount {
     return this.#userRequestedMenus.getTotalMenuPrice() - discountSum;
   }
 
+  /**
+   * @returns {string}
+   */
   calculateEventBadge() {
     const benefitsAmount = this.calculateTotalBenefitsAmount();
 
@@ -47,6 +78,11 @@ class Discount {
     return BADGE.nothing;
   }
 
+  /**
+   * @returns {object}
+   * @property {string} weekly
+   * @property {number} amount
+   */
   #calculateWeeklyDiscount() {
     if (this.#visitDate.isWeekdays()) {
       return {
@@ -61,6 +97,9 @@ class Discount {
     };
   }
 
+  /**
+   * @returns {number}
+   */
   #calculateSpecialDisCount() {
     if (this.#visitDate.isSpecialDiscount()) {
       return DISCOUNT.special;
@@ -69,6 +108,9 @@ class Discount {
     return DISCOUNT.nothing;
   }
 
+  /**
+   * @returns {number}
+   */
   #calculateDDayDiscount() {
     if (this.#visitDate.isChristmasDDayDiscount()) {
       return DISCOUNT.dDay.default + (this.#visitDate.getDate() - 1) * DISCOUNT.dDay.unitAmount;
@@ -77,6 +119,9 @@ class Discount {
     return DISCOUNT.nothing;
   }
 
+  /**
+   * @returns {number}
+   */
   #calculateGiftMenuDiscount() {
     if (this.#userRequestedMenus.isGiftMenuAvailable()) {
       return DISCOUNT.giftMenu;
